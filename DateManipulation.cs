7d6d93221cda3017dcd647a3f0ab6325b9bb8a4c
roku674 <af03848@georgiasouthern.edu>
2022-08-31 17:50:40 -0400

    Create DateManipulation.cs

---
+++ DateManipulation.cs
@@ -0,0 +1,35 @@
+﻿//It's not what it sounds like I swear!
+//Created by Alexander Fields https://github.com/roku674
+
+using System;
+
+namespace Algorithms
+{
+    public static class DateManipulation
+    {
+        /// Extension method parsing a date string to a DateTime? <para/>
+        /// <summary>
+        /// </summary>
+        /// <param name="dateTimeStr">The date string to parse</param>
+        /// <param name="dateFmt">dateFmt is optional and allows to pass
+        /// a parsing pattern array or one or more patterns passed
+        /// as string parameters</param>
+        /// <returns>Parsed DateTime or null</returns>
+        public static DateTime? ToDate(this string dateTimeStr, params string[] dateFmt)
+        {
+            // example: var dt = "2011-03-21 13:26".ToDate(new string[]{"yyyy-MM-dd HH:mm",
+            //                                                  "M/d/yyyy h:mm:ss tt"});
+            // or simpler:
+            // var dt = "2011-03-21 13:26".ToDate("yyyy-MM-dd HH:mm", "M/d/yyyy h:mm:ss tt");
+            const System.Globalization.DateTimeStyles style = System.Globalization.DateTimeStyles.AllowWhiteSpaces;
+            if (dateFmt == null)
+            {
+                var dateInfo = System.Threading.Thread.CurrentThread.CurrentCulture.DateTimeFormat;
+                dateFmt = dateInfo.GetAllDateTimePatterns();
+            }
+            var result = DateTime.TryParseExact(dateTimeStr, dateFmt, System.Globalization.CultureInfo.InvariantCulture,
+                           style, out var dt) ? dt : null as DateTime?;
+            return result;
+        }
+    }
+}
