7b9953b57362e2b46611167a675e09980b4b1ebe
roku674 <af03848@georgiasouthern.edu>
2022-09-02 23:19:01 -0400

    Create Mathematics.cs

---
+++ Mathematics.cs
@@ -0,0 +1,47 @@
+﻿//Created by Alexander Fields https://github.com/roku674
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Algorithms
+{
+    internal class Mathematics
+    {
+        private List<Point> SortByDistance(List<Point> lst)
+        {
+            List<Point> output = new List<Point>();
+            output.Add(lst[NearestPoint(new Point(0, 0), lst)]);
+            lst.Remove(output[0]);
+            int x = 0;
+            for (int i = 0; i < lst.Count + x; i++)
+            {
+                output.Add(lst[NearestPoint(output[output.Count - 1], lst)]);
+                lst.Remove(output[output.Count - 1]);
+                x++;
+            }
+            return output;
+        }
+
+        private int NearestPoint(Point srcPt, List<Point> lookIn)
+        {
+            KeyValuePair<double, int> smallestDistance = new KeyValuePair<double, int>();
+            for (int i = 0; i < lookIn.Count; i++)
+            {
+                double distance = Math.Sqrt(Math.Pow(srcPt.X - lookIn[i].X, 2) + Math.Pow(srcPt.Y - lookIn[i].Y, 2));
+                if (i == 0)
+                {
+                    smallestDistance = new KeyValuePair<double, int>(distance, i);
+                }
+                else
+                {
+                    if (distance < smallestDistance.Key)
+                    {
+                        smallestDistance = new KeyValuePair<double, int>(distance, i);
+                    }
+                }
+            }
+            return smallestDistance.Value;
+        }
+    }
+}
